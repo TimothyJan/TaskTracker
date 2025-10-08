44f3b6036f861738059fa2115ed6af5b04abd40a
Timothy jan <jan.timothy@gmail.com>
2025-10-07 18:13:57 -0700

    Updated role and employee services for http

--- TaskTracker.client/src/app/services/role-service.ts
+++ TaskTracker.client/src/app/services/role-service.ts
@@ -1,88 +1,52 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Role } from '../models/role.model';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { ApiResponse } from '../models/apiResponse.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleService {
+  private apiUrl = `${environment.apiUrl}/api/Role`;
   private rolesChangedSource = new Subject<void>();  // Emit events when role is updated
   rolesChanged$ = this.rolesChangedSource.asObservable();
-
-  roleId: number = 10;
-
-  roles: Role[] = [
-    new Role(0, "ACCOUNTANT", 0),
-    new Role(1, "FINANCIAL ANALYST", 0),
-    new Role(2, "FINANCE MANAGER", 0),
-    new Role(3, "HR ASSISTANT", 1),
-    new Role(4, "HR SPECIALIST", 1),
-    new Role(5, "HR DIRECTOR", 1),
-    new Role(6, "SOFTWARE ENGINEER", 2),
-    new Role(7, "FRONT-END DEVELOPER", 2),
-    new Role(8, "BACK-END DEVELOPER", 2),
-    new Role(9, "FULL-STACK DEVELOPER", 2),
-  ];
-
-  constructor() { }
-
-  /** Get Roles */
-  getRoles(): Role[] {
-    return this.roles;
-  }
-
-  /** Get Roles based on DepartmenIdd */
-  getRolesFromDepartmentId(departmentId: number): Role[] {
-    return this.roles.filter(role => role.departmentId === departmentId);
-  }
-
-  /** Get Role based on id */
-  getRoleById(id: number): Role | undefined {
-    for(let i=0; i<this.roles.length; i++) {
-      if(this.roles[i].id == id) {
-        return this.roles[i];
-      }
-    }
-    return undefined;
-  }
-
-  /** Post new Role */
-  createRole(role: Role): void {
-    let newRole = new Role(this.roleId++, role.name_.toUpperCase(), role.departmentId);
-    this.roles.push(newRole);
-  }
-
-  /** Update existing Role based on id */
-  updateRole(role: Role): void {
-    let updatedRole = new Role(role.id, role.name_.toUpperCase(), role.departmentId);
-    for(let i=0; i<this.roles.length; i++) {
-      if(this.roles[i].id == role.id) {
-        this.roles[i] = updatedRole;
-      }
-    }
-  }
-
-  /** Delete Role based on id */
-  deleteRole(id: number): void {
-    for(let i=0; i<this.roles.length; i++) {
-      if(this.roles[i].id == id) {
-        this.roles.splice(i, 1);
-      }
-    }
-  }
 
   /** Emit events for roles update */
   notifyRolesChanged(): void {
     this.rolesChangedSource.next();
   }
 
-  /** Checks for duplicate department name_s */
-  checkDuplicates(role: Role, excludeRoleId?: number): boolean {
-    const upperName = role.name_.toUpperCase();
-    return this.roles.some(existingRole =>
-      existingRole.name_.toUpperCase() === upperName &&
-      existingRole.departmentId === role.departmentId &&
-      existingRole.id !== excludeRoleId
-    );
+  constructor(private http: HttpClient) { }
+
+  /** Get Roles */
+  getRoles(): Observable<ApiResponse<Role[]>> {
+    return this.http.get<ApiResponse<Role[]>>(this.apiUrl);
+  }
+
+  /** Get Roles based on id */
+  getRoleById(id: number): Observable<ApiResponse<Role>> {
+    return this.http.get<ApiResponse<Role>>(`${this.apiUrl}/${id}`);
+  }
+
+  /** Post new Role */
+  createRole(role: Role): Observable<ApiResponse<Role>> {
+    return this.http.post<ApiResponse<Role>>(this.apiUrl, role);
+  }
+
+  /** Update existing Role based on id */
+  updateRole(role: Role): Observable<ApiResponse> {
+    return this.http.put<ApiResponse>(`${this.apiUrl}/${role.id}`, role);
+  }
+
+  /** Delete Role based on id */
+  deleteRole(id: number): Observable<ApiResponse> {
+    return this.http.delete<ApiResponse>(`${this.apiUrl}/${id}`);
+  }
+
+  /** Get Roles based on Department Id */
+  getRolesFromDepartmentId(departmentId: number): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(`${this.apiUrl}/Role/department/${departmentId}`);
   }
 }
